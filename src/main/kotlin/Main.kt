59197b2d0237b159ac1155aca5580dd32ce3e1f8
Patrick Pichler <ppichler@outlook.at>
2024-12-15 22:31:19 +0100

    feat: repl

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,7 @@
 fun main() {
-    // Uncomment this block to pass the first stage
-     print("$ ")
-
-    val input = readln() // Wait for user input
-
-    println("${input}: command not found")
+    while (true) {
+        print("$ ")
+        val input = readln() // Wait for user input
+        println("$input: command not found")
+    }
 }
