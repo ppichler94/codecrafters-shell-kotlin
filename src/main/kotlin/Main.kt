6a84eb648124139bdd9dac4f72e4f14dd59f50b0
Patrick Pichler <ppichler@outlook.at>
2024-12-16 18:30:25 +0100

    refactor: implement builtins as sealed classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,48 @@
-fun main() {
-    val builtins = setOf("echo", "type", "exit")
+import kotlin.system.exitProcess
 
+sealed interface Command {
+    operator fun invoke(arguments: List<String>)
+}
+
+data object Echo : Command {
+    override fun invoke(arguments: List<String>) = println(arguments.joinToString(" "))
+}
+
+data object Type : Command {
+    override fun invoke(arguments: List<String>) {
+        if (arguments.first() in CommandNames.entries.map { it.value }) {
+            println("${arguments.first()} is a shell builtin")
+        } else {
+            println("${arguments.first()}: not found")
+        }
+    }
+}
+
+data object Exit : Command {
+    override fun invoke(arguments: List<String>) = exitProcess(0)
+}
+
+enum class CommandNames(
+    val value: String,
+    val command: Command,
+) {
+    ECHO("echo", Echo),
+    EXIT("exit", Exit),
+    TYPE("type", Type),
+}
+
+fun main() {
     while (true) {
         print("$ ")
         val input = readln() // Wait for user input
         val parts = input.split(" ")
 
-        when (parts.first()) {
-            "exit" -> return
-            "echo" -> println(parts.drop(1).joinToString(" "))
-            "type" ->
-                if (parts[1] in builtins) {
-                    println("${parts[1]} is a shell builtin")
-                } else {
-                    println("${parts[1]}: not found")
-                }
-            else -> println("$input: command not found")
+        val command = CommandNames.entries.find { it.value == parts.first() }?.command
+
+        if (command != null) {
+            command(parts.drop(1))
+        } else {
+            println("$input: command not found")
         }
     }
 }
